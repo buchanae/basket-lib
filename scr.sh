03c137966ce5d24db0d5bc4feaad901808b2a807
Alex Buchanan <abuchanan@mozilla.com>
2010-06-14 17:57:44 -0700

    add httplib2

--- scr.sh
+++ scr.sh
@@ -1,6 +1,13 @@
 #/bin/bash
 
+find packages src -type d -depth 1 >| basket.pth
+perl -pi -e 's:httplib2:httplib2/python2:' basket.pth
+
+git init
+
 for f in src/*
 do
     pushd $f > /dev/null && REPO=$(git config remote.origin.url) && popd > /dev/null && git submodule add $REPO $f
 done
+
+git add .
